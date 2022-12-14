9e3a3bc57b19f0861b38613e91d481bc80336d26
JYK75 <juuyoung9575@gmail.com>
2022-12-15 04:35:32 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import DynamicMap from './Map/DynamicMap';
+import KakaoMapScriptLoader from './Map/KakaoMapScriptLoader';
+import MapMarkerController from './Map/MapMarkerController';
+import { PlaceType } from './Map/mapTypes';
+import SearchLocation from './Map/SearchLocation';
 
 function App() {
+  const [places, setPlaces] = useState<PlaceType[]>([]);
+  const [selectedPlaceId, setSelectedPlaceId] = useState('');
+
+  console.log(places)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <KakaoMapScriptLoader>
+      <DynamicMap>
+        <MapMarkerController places={places} selectedPlaceId={selectedPlaceId}/>
+        <SearchLocation onUpdatePlaces={(places) => {
+          setPlaces(places);
+        }} onSelect={(placeId) => {
+          setSelectedPlaceId(placeId)
+        }}/>
+      </DynamicMap>
+    </KakaoMapScriptLoader>
   );
 }
 
